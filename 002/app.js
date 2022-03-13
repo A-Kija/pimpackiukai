1dc37aaad4f96e0f7d58121cbeacb8d3aab56411
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-13 11:18:25 +0200

    sticker

--- 002/app.js
+++ 002/app.js
@@ -14,3 +14,25 @@ const namas2 = [
 const gamers = ['Vytas', 'Lina', 'Bebras', 'Žirafas', 'Nausėda', 'Teta Zita'];
 
 console.table(namas2[0].pot);
+
+// =======================
+
+const try10 = []; // nauja deze
+
+for (let i = 0; i < 3; i++) {
+    const gamer = {}; // nauja deze
+    gamer.name = gamers[rand(0, gamers.length - 1)];
+    gamer.try = rand(1, 6);
+    try10.push(gamer);
+}
+
+console.table(try10);
+
+// const red = { name: 'Berbras', vazonas: 1 };
+
+// const pink = red;
+
+// red.name = 'Teta Zita';
+
+// console.table('RED:', red);
+// console.table('PINK:', pink);
