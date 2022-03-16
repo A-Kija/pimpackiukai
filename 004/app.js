306fd316939ffe6ac37620e48b214364e42a9c2a
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-16 18:42:11 +0200

    users

--- 004/app.js
+++ 004/app.js
@@ -13,11 +13,39 @@
 // console.log('aj', typeof aj, aj);
 
 
+console.log(document.querySelector('h1'));
 
 
-fetch('https://jsonplaceholder.typicode.com/posts/99')
+// fetch('https://jsonplaceholder.typicode.com/posts/99')
+//     .then(response => response.json())
+//     .then(data => {
+//         document.querySelector('h1').innerText = data.title;
+//         document.querySelector('p').innerText = data.body;
+//         //cia
+
+//         console.log(data)
+//     });
+
+// let t = 0;
+
+// const time = setInterval(() => {
+//     console.log(++t);
+//     if (t > 40) {
+//         clearInterval(time);
+//     }
+// }, 1);
+
+const ul = document.querySelector('ul');
+
+fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        data.forEach(element => {
+            const li = document.createElement('li');
+            const text = document.createTextNode(element.username);
+            li.appendChild(text);
+            ul.appendChild(li);
+        });
 
-
-console.log('Labas');
+        console.log(data)
+    });
