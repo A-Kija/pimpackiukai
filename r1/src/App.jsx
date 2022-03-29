295d3bf064d6c0a1f1464d809bea177ca83e4726
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-29 18:21:55 +0300

    r1

---
+++ r1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Labas from './Components/Labas';
+
+function App() {
+  return (
+    <div className="App">
+      <Labas/>
+      <Labas/>
+      <span>Bla bla bla</span>
+      <Labas></Labas>
+    </div>
+  );
+}
+
+export default App;
---
+++ r1/src/Components/Labas.jsx
@@ -0,0 +1,13 @@
+
+function Labas() {
+    return (
+        <h1 style={{
+            color: 'pink',
+            fontSize: '13px'
+        }}>Labas, Zuiki!</h1>
+    )
+    
+}
+
+
+export default Labas;
