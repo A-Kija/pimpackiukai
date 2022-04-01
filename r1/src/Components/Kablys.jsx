96e9926d7bb5a6e34e127b23678638f7b4fb9988
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-01 20:37:43 +0300

    FS

--- r1/src/Components/Kablys.jsx
+++ r1/src/Components/Kablys.jsx
@@ -2,16 +2,31 @@ import { useState } from 'react';
 function Kablys({color}) {
 
     const [nr, setNr] = useState(11);
+    const [fs, setFs] = useState(20);
 
 
     const changeNr = () => {
         setNr(257);
     }
 
+    const changeFs = () => {
+        // if (fs == 20) {
+        //     setFs(40);
+        // } else {
+        //     setFs(20)
+        // }
+        setFs(oldFs => oldFs == 20 ? 40 : 20);
+    }
+
     return (
         <>
-        <div style={{color: color}}>Aš Kablys Nr. {nr} </div>
+        <div style={{
+            color: color,
+            fontSize: fs + 'px'
+            
+            }}>Aš Kablys Nr. {nr} </div>
         <button onClick={changeNr}>GO</button>
+        <button onClick={changeFs}>FS</button>
         </>
     )
 }
