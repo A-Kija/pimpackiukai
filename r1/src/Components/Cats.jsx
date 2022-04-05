27b113fbb86818af992a150c063e1ca39b54895f
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-05 21:04:33 +0300

    katinukai

--- r1/src/Components/Cats.jsx
+++ r1/src/Components/Cats.jsx
@@ -4,9 +4,10 @@ function Cats() {
 
     const [cat, setCat] = useState('');
     const [catsList, setCatsList] = useState([]);
+    const [catColor, setCatColor] = useState('brown');
 
     const add = () => {
-        setCatsList(c => [...c, cat]);
+        setCatsList(c => [...c, {cat, catColor}]);
         setCat('');
     }
 
@@ -14,15 +15,21 @@ function Cats() {
     return (
         <>
         <button onClick={add}>Add Cat</button>
+        <select onChange={e => setCatColor(e.target.value)} value={catColor}>
+            <option value="brown">Brown</option>
+            <option value="gray">Gray</option>
+            <option value="bisque">Biskvitas</option>
+        </select>
         <input onChange={e => setCat(e.target.value)} value={cat}/>
         <div className="kvc">
         {
             catsList.map((c,i) => {
                 return (<div key={i} className="cat" style={{
-                    width: (c.length * 14) + 10 + 'px',
-                    height: (c.length * 14) + 10 + 'px'
+                    width: (c.cat.length * 14) + 10 + 'px',
+                    height: (c.cat.length * 14) + 10 + 'px',
+                    background: c.catColor
                 }}>
-                    {c}
+                    {c.cat}
                 </div>
                 )
             })
