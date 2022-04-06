dda40b410b7d954bfdf4a8dfb61aa426061bf514
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-06 19:30:17 +0300

    ACDC

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
         <h1>RePEAT</h1>
-        <A letter="A" letter1="B" letter2="C" letter3="D"></A>
+        <A letter="A" letter1="C" letter2="D" letter3="C"></A>
     </div>
   );
 }
