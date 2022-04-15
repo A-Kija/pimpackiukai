5edff7c5474b218501811497490e19ae5320e0c7
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-15 18:52:30 +0300

    viskas

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,4 +1,5 @@
 import { useReducer, useState } from 'react';
+import { add1, rem1 } from './Actions/basic';
 import './App.css';
 import c2Reducer from './Reducers/c2Reducer';
 
@@ -12,11 +13,18 @@ function App() {
         <div className="App">
             <h1>ReDUCeR</h1>
             <h2>state count: {c1}</h2>
+            <h2>state count: {c2}</h2>
 
             <fieldset>
                 <legend>STATE</legend>
                 <button onClick={() => setC1(c => c + 1)}>+1</button>
                 <button onClick={() => setC1(c => c - 1)}>-1</button>
+            </fieldset>
+
+            <fieldset>
+                <legend>REDUCER</legend>
+                <button onClick={() => dispachC2(add1())}>+1</button>
+                <button onClick={() => dispachC2(rem1())}>-1</button>
             </fieldset>
 
         </div>
--- r1/src/Reducers/c2Reducer.js
+++ r1/src/Reducers/c2Reducer.js
@@ -1,7 +1,19 @@
+import { ADD_1, REM_1 } from "../Constants";
+
 function c2Reducer(state, action) {
+    let c = state; // steito kopija
 
+    switch (action.type) {
+        case ADD_1:
+            c++;
+            break;
+        case REM_1:
+            c--;
+            break;
+        default:
+    }
 
-    return state;
+    return c;
 }
 
 export default c2Reducer;
