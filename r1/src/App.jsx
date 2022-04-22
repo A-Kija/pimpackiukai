10a1dd721eb366083d100424b929d57e319d96d7
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-22 20:05:45 +0300

    404

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -7,14 +7,17 @@ function App() {
 
             <div className="App">
                 <h1>ROU-TER</h1>
+                <Link to="/">Home Page</Link>
                 <Link to="/dramblys">Dramblys</Link>
                 <Link to="/pingvinas">Pingvinas</Link>
             </div>
 
             <div className="kvc">
             <Routes>
+                <Route path="/" element={<h2>HOME</h2>}></Route>
                 <Route path="/dramblys" element={<div className="kv kv_3">DRABLIUS</div>}></Route>
                 <Route path="/pingvinas" element={<div className="kv kv_1">PINVIUS</div>}></Route>
+                <Route path="*" element={<h2>404 NOT FOUND</h2>}></Route>
             </Routes>
             </div>
 
