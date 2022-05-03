2da32a634626f49d183b88dba62a882855818fa0
A-Kija <arvydas.kijakauskas@gmail.com>
2022-05-03 19:11:38 +0300

    getCartTotal

--- r1/src/Functions/Books.js
+++ r1/src/Functions/Books.js
@@ -10,8 +10,20 @@ class Books {
 
     static getCartTotal(cart, booksStore) {
         let amount = 0;
-        // 
+        cart.forEach(c => {
+            amount += this.getBookById(c.id, booksStore).price * c.count;
+        });
         return amount; // float eurais
+    }
+
+    static getBookById(id, booksStore) {
+        let index = -1;
+        booksStore.forEach((b, i) => {
+            if (id === b.id) {
+                index = i;
+            }
+        });
+        return booksStore[index];
     }
 
 }
